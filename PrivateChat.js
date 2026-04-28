cc15479e1495fab62879309a24c324d13b7dfc6e
DKJL0527 <desmondkohjiale@gmail.com>
2026-04-28 16:08:06 +0800

    Update and rename chatsystem.js to PrivateChat.js

---
+++ PrivateChat.js
@@ -0,0 +1,46 @@
+module.exports = (socket, io, UsersList) => {
+
+  socket.on('send messange', async (data) => {
+    const { senderid, receiverid, messange } = data;
+
+    try {
+    
+      await MessageModel.create({
+        sender: senderid,
+        receiver: receiverid,
+        content: messange,
+        status: 'delivered',
+        date_time: new Date().toLocaleString()
+      });
+
+    
+      const receiverSocketID = UsersList[receiverid];
+
+      if (receiverSocketID) {
+        io.to(receiverSocketID).emit('receive messange', {
+          senderid,
+          messange,
+          Date_and_Time: new Date().toLocaleString()
+        });
+        console.log(`Real-time delivery to: ${receiverid}`);
+      } else {
+        console.log(`User ${receiverid} is offline. Message archived.`);
+      }
+
+    } catch (error) {
+      console.error("Critical Error:", error);
+      socket.emit('error', {
+        system_messange: "messange send failed, please try again"
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const userid = Object.keys(UsersList).find(k => UsersList[k] === socket.id);
+    
+    if (userid) {
+      delete UsersList[userid]; 
+      console.log(`[Status] ${userid} is now offline. UsersList cleaned.`);
+    }
+  });
+};
