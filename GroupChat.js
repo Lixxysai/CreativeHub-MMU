e965f60f475f3f0bbf139d9967565f25e90ce20b
DKJL0527 <desmondkohjiale@gmail.com>
2026-05-04 13:42:11 +0800

    Update GroupChat.js

--- GroupChat.js
+++ GroupChat.js
@@ -1,31 +1,58 @@
 module.exports = (socket, io, UsersList) => {
 
-        socket.on("join group", async (data) => {
-        const { userid, groupid } = data;
-        
-        socket.join(groupid);
+  socket.on("join group", async (data) => {
+    const { userid, groupid } = data;
+    socket.join(groupid);
 
-        console.log('User ${userid} is join ${groupid}');
+    console.log(`User ${userid} joined ${groupid}`);
 
-        io.to(groupid).emit("Someone join this group", {
-          System: "Welcome $(userid) to $(groupid)",
-        });
+    io.to(groupid).emit("group notification", {
+      System: `Welcome ${userid} to ${groupid}`,
     });
+  });
 
-    socket.on("leave group", (data) => {
-        const { userid, groupid} = data;
-        socket.leave(groupid);
+  socket.on("send group message", async (data) => {
+    const { senderid, groupid, message } = data; // Defined here
 
-        console.log('User ${userid} is leave ${groupid}');
+    try {
+      const savedMessage = await MessageModel.create({
+        sender: senderid,
+        groupid: groupid,
+        content: message,
+        status: 'delivered',
+        date_time: new Date().toLocaleString()
+      });
 
-        io.to(groupid).emit("Someone join this group", {
-          System: "$(userid) left the group",
-        });
+      io.to(groupid).emit('receive group message', {
+        senderid: senderid,
+        message: message,
+        Date_and_Time: savedMessage.date_time
+      });
 
-    socket.on("disconnect", () => {
-        const userid = Object.keys(UsersList).find(k => UsersList[k] === socket.id);
-        
-        if (userid) {
-            delete UsersList[userid]; 
-            console.log(`${userid} is offline`);
-});
+    } catch (error) {
+      console.error("Critical Error:", error);
+      socket.emit('error', {
+        system_message: "Message send failed, please try again"
+      });
+    }
+  });
+
+  socket.on("leave group", (data) => {
+    const { userid, groupid } = data;
+    socket.leave(groupid);
+
+    console.log(`User ${userid} left ${groupid}`);
+
+    io.to(groupid).emit("group notification", {
+      System: `${userid} left the group`,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    const userid = Object.keys(UsersList).find(k => UsersList[k] === socket.id);
+    if (userid) {
+      delete UsersList[userid];
+      console.log(`${userid} is offline`);
+    }
+  });
+};
