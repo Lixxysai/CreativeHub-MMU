627ea55b584bdcf87ee8ded9e67db76b7bb4b088
DKJL0527 <desmondkohjiale@gmail.com>
2026-06-27 10:34:00 +0800

    Update status.js

--- status.js
+++ status.js
@@ -1,65 +1,63 @@
 module.exports = (socket, io, UsersList) => {
 
-   // online status
   socket.on('online', (username) => {
-        socket.username = username;          
-        UsersList[username] = { id: socket.id, status: "online" }; 
+      socket.username = username;          
+      
+      UsersList[username] = { socketId: socket.id, status: "online" }; 
 
       io.emit('status_change', {
-        username: username,
-        status: "online"
-      })
+          username: username,
+          status: "online"
+      });
   });
 
-   // busy status
   socket.on('busy', () => {
-          const username = socket.username;
-          if (username && UsersList[username]) {
+      const username = socket.username;
+      if (username && UsersList[username]) {
           UsersList[username].status = "busy";
- 
-        io.emit('status_change',{
-          username: username,
-          status: "do not disturb"
-        });
+
+          io.emit('status_change', {
+              username: username,
+              status: "do not disturb"
+          });
       }
   });
 
-   // idle or afk status
   socket.on('afk', () => {
-          const username = socket.username;
-          if (username && UsersList[username]) {
+      const username = socket.username;
+      if (username && UsersList[username]) {
           UsersList[username].status = "afk";
- 
-        io.emit('status_change',{
-          username: username,
-          status: "AFK"
-        });
+
+          io.emit('status_change', {
+              username: username,
+              status: "AFK"
+          });
       }
   });
 
-  // invisible status
   socket.on('invisible', () => {
-          const username = socket.username;
-          if (username && UsersList[username]) {
+      const username = socket.username;
+      if (username && UsersList[username]) {
           UsersList[username].status = "invisible";
- 
-        io.emit('status_change',{
-          username: username,
-          status: "offline"
-        });
-      };
+
+          io.emit('status_change', {
+              username: username,
+              status: "offline"
+          });
+      }
   });
 
-   // offline status
-  socket.on('disconnect',() => {
-          const username = socket.username;
-          if (username && UsersList[username]) {
-          delete UsersList[username]
+  socket.on('disconnect', () => {
+      const username = socket.username;
+      if (username && UsersList[username]) {
           
-        io.emit('status_change',{
-          username: username,
-          status: "offline"
-       });
+          delete UsersList[username]; 
+          
+          io.emit('status_change', {
+              username: username,
+              status: "offline"
+          });
+          console.log(`${username} offline, delete already。`);
       }
   });
 };
