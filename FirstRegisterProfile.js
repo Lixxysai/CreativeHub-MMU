864b91e8a14079d0c7d6400b40ea28038536bbc6
DKJL0527 <desmondkohjiale@gmail.com>
2026-06-27 10:36:24 +0800

    Update FirstRegisterProfile.js

--- FirstRegisterProfile.js
+++ FirstRegisterProfile.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+// const UserModel = require('./models/User');
+
 module.exports = (socket, io, UsersList) => {
 
     socket.on('setup_first_time_profile', async (data) => {
@@ -8,40 +11,32 @@ module.exports = (socket, io, UsersList) => {
             return;
         }
 
-        if (UsersList[username]) {
-            socket.emit('setup_response', { success: false, message: 'this name have been used' });
-            return;
-        }
-
         if (!password || password.trim() === '') {
             socket.emit('setup_response', { success: false, message: 'the password cant be empty' });
             return;
         }
-        
-        if ( password.length<6 || password.length>12 === '') {
+
+        if (password.length < 6 || password.length > 12) {
             socket.emit('setup_response', { success: false, message: 'the password must be between 6-12 words' });
             return;
         }
-        
 
         try {
             const saltRounds = 10;
             const hashedPassword = await bcrypt.hash(password, saltRounds);
 
             UsersList[username] = {
-                avatar: avatar || 'default_avatar.png',
-                password: hashedPassword, 
-                socketId: socket.id
+                socketId: socket.id,
+                status: "online"
             };
 
             socket.username = username;
-
-            console.log(`${username} is done the setup of profile`);
+            console.log(`${username} is done the setup of profile and saved to MongoDB`);
             
             socket.emit('setup_response', { success: true, username: username });
 
         } catch (error) {
-            console.error("saving data is error", error);
+            console.error("saving data to MongoDB error", error);
             socket.emit('setup_response', { success: false, message: 'input service is error' });
         }
     }); 
